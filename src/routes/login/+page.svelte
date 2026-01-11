251b0fe0116b1cf79700c2108427616d4173de08
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2026-01-11 14:18:22 -0500

    Style the login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,25 @@
-<style>
-    .error {
-        color: red;
-    }
-</style>
-
 <script>
     export let form;
 </script>
 
-<h1>Welcome to login</h1>
+<div class="flex justify-center items-center h-screen flex-col gap-10">
+    <div>
+        <h1 class="text-4xl">Welcome to Courier's Game Shelf</h1>
 
-{#if form?.error}
-    <p class="error">{form.message}</p>
-{/if}
+        {#if form?.error}
+            <p class="text-red-500 text-center">{form.message}</p>
+        {/if}
+    </div>
 
-<form method="POST">
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required><br><br>
+    <div>
+        <form method="POST" class="card w-full max-w-md preset-filled-surface-100-900 p-10 text-center">
+            <label for="username" class="text-xl mr-4">Username:</label>
+            <input type="text" id="username" name="username" class="bg-gray-500 rounded-md p-2" required><br><br>
 
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" required><br><br>
+            <label for="password" class="text-xl mr-4">Password:</label>
+            <input type="password" id="password" name="password" class="bg-gray-500 rounded-md p-2" required><br><br>
 
-    <input type="submit" value="Login">
-</form>
+            <input type="submit" value="Login" class="btn present-filled bg-white text-black">
+        </form>  
+    </div>  
+</div>
